188bc6651b932d878d5ac43e719908a18a08df49
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-04-09 05:55:58 +0000

    一覧画面を表示しよう完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @book = Book.new
   end
-  
+
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     book = Book.new(book_params)
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -21,11 +22,11 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
    private
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
