aafdc6c6695f1e16e2f5443d84e8e37139259f64
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-04-10 10:21:30 +0000

    バリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,13 @@ class BooksController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-     # 詳細画面へリダイレクト
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
