029c46f6c08b8823e4e7f5ac725d807e77cc0236
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-04-12 07:19:59 +0000

    削除機能修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,13 +34,13 @@ class BooksController < ApplicationController
 
   def update
 
-    book = Book.find(params[:id])
-    if  book.update(book_params)
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
 
     else
-       @book = Book.find(params[:id])
+       @books = Book.all
        flash[:notice] = "Update failed"
         render :edit
     end
@@ -49,6 +49,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
+
     redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   get 'homes/top'
-  get 'books/new'
   resources :books
 end
